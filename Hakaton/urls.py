d369971838209dec80d9a7087dd7345966764d69
Your Name <you@example.com>
2022-05-14 15:52:51 +0300

    add login

--- Hakaton/urls.py
+++ Hakaton/urls.py
@@ -15,12 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 from django.conf import settings
 from django.conf.urls.static import static
 
+# from blog.views import PostCreateView
+from users import views as u_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', u_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', u_views.profile, name='profile'),
     path('', include('blog.urls')),
 ]
 if settings.DEBUG:
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from users.models import Profile
+
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -10,3 +19,27 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
+@login_required
+def profile(request): #if we login we can get in profile
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+            'user_form': user_form,
+            'profile_form': profile_form
+        }
+    return render(request, 'users/profile.html', context)
